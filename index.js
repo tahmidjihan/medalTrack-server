176ee6a019c7d6851a50531da82b3da9bcb4a8b8
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-02-12 01:26:07 +0600

    offed the jwt

--- index.js
+++ index.js
@@ -10,25 +10,27 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://medal-track.web.app',
+    // origin: 'https://medal-track.web.app',
     credentials: true,
+    // secured: true,
   })
 );
 app.use(express.json());
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies?.token;
-  if (!token) {
-    return res.status(401).send('Unauthorized');
-  }
+  // if (!token) {
+  //   return res.status(401).send('Unauthorized');
+  // }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send('Invalid or expired token');
-    }
-    req.user = decoded;
-    next();
-  });
+  // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+  //   if (err) {
+  //     return res.status(403).send('Invalid or expired token');
+  //   }
+  //   req.user = decoded;
+  //   next();
+  // });
+  next();
 };
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tfnar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
